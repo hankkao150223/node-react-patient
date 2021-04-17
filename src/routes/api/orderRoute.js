ddb82807f1c2d87836e432e7242637dadd2bfb93
Hank <hankkao1475369@gmail.com>
2021-04-18 00:34:59 +0800

    feat: remove 增加醫囑

--- src/routes/api/orderRoute.js
+++ src/routes/api/orderRoute.js
@@ -10,30 +10,4 @@ const orderSchema = Joi.object({
   message: Joi.string().trim().required(),
 });
 
-router.post('/', async (req, res) => {
-  const { error, value: order } = orderSchema.validate(req.body);
-  if (error) {
-    res.status(400).send({
-      isSuccess: false,
-      message: error.details[0].message,
-    });
-    return;
-  }
-  try {
-    const orderFromDb = await Order.create(order);
-    res.status(201).json({
-      isSuccess: true,
-      data: {
-        id: orderFromDb._id,
-        message: orderFromDb.message,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      isSuccess: false,
-      message: err.message,
-    });
-  }
-});
-
 module.exports = router;
