d9f29d72b3f34e3fdb5b9577c1912505c2760dda
Hank <hankkao1475369@gmail.com>
2021-04-17 12:58:25 +0800

    feat: get api/patient

--- src/routes/api/patientRoute.js
+++ src/routes/api/patientRoute.js
@@ -1,11 +1,22 @@
+const mongoose = require('mongoose');
 const express = require('express');
+
+const Patient = mongoose.model('patients');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    test: 123,
-  });
+router.get('/', async (req, res) => {
+  try {
+    const patients = await Patient.find();
+    res.send({
+      patients,
+    });
+  } catch (err) {
+    res.status(500).send({
+      err: err.message,
+      patients: [],
+    });
+  }
 });
 
 module.exports = router;
